26458d32b1299c2b6db455db3337e032139f0e5a
JCJV2019 <cjordanvergara@outlook.es>
2019-05-29 00:30:23 +0200

    primera parte

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,42 +1,96 @@
-function deleteItem(e){
-
+function creaLineaProducto() {
+    let contenedor = document.getElementById('lpcontainer');
+    contLineas++;
+    creaContenedor(contenedor);
+    contenedor = document.getElementById('linea'+contLineas);
+    creaProducto(contenedor);
+    creaPrecio(contenedor);
+    creaQTY(contenedor);
+    creaQTYInput(contenedor);
+    creaTotalLinea(contenedor);
+    creaBotonDelete(contenedor);
 }
 
-function getPriceByProduct(itemNode){
-
+function creaContenedor(contenedor) {
+    // Contenedor de Linea del Producto
+    let element = document.createElement('div');
+    contenedor.appendChild(element);
+    element.setAttribute('id','linea'+contLineas);
+    element.setAttribute('class','linea');
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function creaProducto(contenedor) {
+    let producto = document.getElementById('producto');
+    let element = document.createElement('span');
+    contenedor.appendChild(element);
+    element.innerText = producto.value;
 }
 
-function getTotalPrice() {
-
+function creaPrecio(contenedor) {
+    let precio = document.getElementById('precio');
+    let element = document.createElement('span');
+    contenedor.appendChild(element);
+    element.innerText = precio.value;
+    element.setAttribute('class','precios');
 }
 
-function createQuantityInput(){
-
+function creaQTY(contenedor) {
+    let element = document.createElement('span');
+    contenedor.appendChild(element);
+    element.innerText = 'QTY';
 }
 
-function createDeleteButton(){
-
+function creaQTYInput(contenedor) {
+    let element = document.createElement('input');
+    contenedor.appendChild(element);
+    element.setAttribute('class','input');
+    element.setAttribute('type','text');
 }
 
-function createQuantityNode(){
-
+function creaTotalLinea(contenedor) {
+    let element = document.createElement('span');
+    contenedor.appendChild(element);
+    element.innerText = '€';
+    element.setAttribute('class','eurosProducto');
 }
 
-function createItemNode(dataType, itemData){
-
+function creaBotonDelete(contenedor) {
+    let element = document.createElement('div');
+    contenedor.appendChild(element);
+    let SubContenedor = contenedor.getElementsByTagName('div'); 
+    // Solo existe uno
+    element = document.createElement('span');
+    SubContenedor[0].appendChild(element);
+    element.innerText = 'Delete';
+    element.setAttribute('class','btn btn-delete');
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function calculaPrecios() {
+    let contenedor = document.getElementById('lpcontainer');
+    let mLineas = contenedor.getElementsByClassName('linea');
+    let cantidad, precio, totalLinea;
+    for(let i=0; i<mLineas.length; i++) {
+        // Cantidad de Producto
+        contenedor = mLineas[i].getElementsByTagName('input'); 
+        cantidad = contenedor[0].value;
+        // Precio del Producto
+        contenedor = mLineas[i].getElementsByClassName('precios'); 
+        precio = contenedor[0].innerText;
+        // Total Linea
+        totalLinea = parseFloat(precio) * parseFloat(cantidad);
+        contenedor = mLineas[i].getElementsByClassName('eurosProducto'); 
+        contenedor[0].innerText = totalLinea;
 
+    }  
 }
 
-function createNewItem(){
-
-}
-
+let contLineas = 0;
 addEventListener('load', ()=>{
+    // Lanza Evento Boton Create
+    let botonCreate = document.getElementById('bCreate');
+    botonCreate.addEventListener('click', creaLineaProducto);
+    // Lanza Evento Boton bCalculate
+    let botonCalculate = document.getElementById('bCalculate');
+    botonCalculate.addEventListener('click', calculaPrecios);
+
 });
